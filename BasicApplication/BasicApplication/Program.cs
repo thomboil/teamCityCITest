b38864130cf1c3b825075903be0895dfa5523487
Boily <Thomas.Boily@wbgames.com>
2020-06-12 11:33:31 -0400

    First commit

---
+++ BasicApplication/BasicApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is a Continuous Integration Test");
+            Console.ReadLine();
+        }
+    }
+}
